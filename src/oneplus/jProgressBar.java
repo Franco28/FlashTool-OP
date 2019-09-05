da60b3dbef8763672980c27011a3ecdf31c52ee4
Franco28 <franco.mato.contin@gmail.com>
2019-09-05 19:46:01 -0300

    Initial

---
+++ src/oneplus/jProgressBar.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oneplus;
+
+/**
+ *
+ * @author Franco
+ */
+class jProgressBar {
+
+    static void setString(String calculando_tamaño_del_archivo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
