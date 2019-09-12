d56e1e71fe361f4171dc9d4fee55b89a64747677
Franco28 <franco.mato.contin@gmail.com>
2019-09-11 23:11:04 -0300

    1.0.1.9-RC1
    - Added ROMS download panel
    - Added OnePlus 5T
    - Added Tool selector at startup
    - Updated download links
    - Fixed English language in the Tool for OnePlus 5
    - Other code fixes

---
+++ src/oneplus/los.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2019 Franco.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package oneplus;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Franco
+ */
+public interface los {
+    
+public class loos extends Application {
+    private Scene scene;
+    @Override public void start(Stage stage) {
+        // create the scene
+        stage.setTitle("Pixel Experience");
+        Image icon = new Image(getClass().getResourceAsStream("pe.png"));
+        stage.getIcons().add(icon);
+        scene = new Scene(new Browser(),850,450, Color.web("#666970"));
+        stage.setScene(scene);      
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
+class Browser extends Region {
+
+    final WebView browser = new WebView();
+    final WebEngine webEngine = browser.getEngine();
+
+    public Browser() {
+        //apply the styles
+        getStyleClass().add("browser");
+        // load the web page
+        webEngine.load("https://download.pixelexperience.org/cheeseburger/");
+        //add the web view to the scene
+        boolean add = getChildren().add(browser);
+
+    }
+    private Node createSpacer() {
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        return spacer;
+    }
+
+    @Override protected void layoutChildren() {
+        double w = getWidth();
+        double h = getHeight();
+        layoutInArea(browser,0,0,w,h,0, HPos.CENTER, VPos.CENTER);
+    }
+
+    @Override protected double computePrefWidth(double height) {
+        return 750;
+    }
+
+    @Override protected double computePrefHeight(double width) {
+        return 500;
+    }
+}
+
+}
